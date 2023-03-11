0cbc64f34b42a693af068c1572ab32bca645cff3
Perry Rylance <perryrylance@googlemail.com>
2023-03-11 12:06:21 +0000

    Clean up

--- src/App.tsx
+++ src/App.tsx
@@ -21,50 +21,8 @@ function App()
 	return <div className="app">
 		<AppContext.Provider value={{state, setState}}>
 			<Chart onStateChanged={onChartStateChange} />
-			<Chart onStateChanged={onChartStateChange} />
 		</AppContext.Provider>
 	</div>;
 }
-
-/*class App extends React.Component
-{
-	constructor(props)
-	{
-		super(props);
-
-		this.state = {
-			semitones:	[true, false, true, false, true, true, false, true, false, true, false, true],
-			key:		0,
-			tuning:		[64, 59, 55, 50, 45, 40],
-			frets:		13
-		};
-
-		const params	= new URLSearchParams(window.location.search);
-
-		if(params.has("state"))
-			this.state	= JSON.parse( Base64.decode(params.get("state")) );
-	}
-
-	
-
-	componentDidUpdate()
-	{
-		const params	= new URLSearchParams(window.location.search);
-		const data		= Base64.encode(JSON.stringify(this.state));
-	
-		params.set("state", data);
-
-		window.history.replaceState(this.state, window.title, ["?" + params.toString()]);
-	}
-
-	render()
-	{
-		return (
-			<div className="app">
-				
-			</div>
-		);
-	}
-}*/
 
 export default App;
